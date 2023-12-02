332c5c35ab29cfacdac4a170ffb54026bb185cdb
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2023-12-02 17:48:24 +0300

    setup.py: Add setup file
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

---
+++ setup.py
@@ -0,0 +1,15 @@
+import sys
+from setuptools import setup
+
+
+setup(
+    name="udbq",
+    version="0.0.0",
+    description="Minimalist Python database query builder / anti-ORM",
+#    long_description=open("README.rst").read(),
+    url="https://github.com/pfalcon/udbq",
+    author="Paul Sokolovsky",
+    author_email="pfalcon@users.sourceforge.net",
+    license="MIT",
+    py_modules=["udbq"],
+)
